1b733c13c4c40f5a64bede9d408c87eb33b32826
Brian Vanderbusch <brian.vanderbusch@gmail.com>
2016-09-27 11:31:13 -0500

    get info from spreadsheet

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-var MCrypt = require('mcrypt').MCrypt;
-var fs = require('fs');
 
-var key = fs.readFileSync('DTCTT.key');
-var iv = fs.readFileSync('DTCTT.iv', 'utf8');
-var proc = fs.readFileSync('DTCTT.proc');
-
-var rijCBC = new MCrypt('rijndael-128', 'cbc');
-
-console.log(rijCBC.isBlockAlgorithm());
